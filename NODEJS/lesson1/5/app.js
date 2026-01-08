700d721f5b0ae69b3c205452cee78db4d67fc76d
Kalashkhatod <kalashkhatod@gmail.com>
2026-01-09 00:24:59 +0530

    Modified app.js

--- NODEJS/lesson1/5/app.js
+++ NODEJS/lesson1/5/app.js
@@ -1,16 +1,36 @@
 const Emitter = require("./emitter");
-
 const emittr = new Emitter();
 
-emittr.on("greet",function(){
-    console.log("Hello");
+const obj = require("./config").events;
+
+// bad practice
+
+// emittr.on("greet",function(){
+//     console.log("Hello");
     
-})
+// })
 
-emittr.on("greet",function(){
-    console.log("Hello1");
+// emittr.on("greet",function(){
+//     console.log("Hello1");
     
-})
+// })
 
-emittr.emit("greet");
+// emittr.emit("greet");
 
+
+emittr.on(obj.GREET, function(){
+    console.log("Greet");
+}
+);
+emittr.on(obj.FILESAVED, function(){
+    console.log("file saved successfully");
+}
+);
+emittr.on(obj.FILEOPEN, function(){
+    console.log("file opened successfully");
+}
+);
+
+emittr.emit(obj.GREET);
+emittr.emit(obj.FILESAVED);
+emittr.emit(obj.FILEOPEN);
