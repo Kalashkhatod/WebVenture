c3bcca2672be47e734d29e19131ba5afb9bb8716
Kalashkhatod <kalashkhatod@gmail.com>
2026-01-27 19:13:27 +0530

    Added basic form submission using Node.js

--- NODEJS/lesson2/8/app.js
+++ NODEJS/lesson2/8/app.js
@@ -1,30 +1,53 @@
+// const http = require("http");
+// const server = http.createServer((req,res) =>{
+//     if(req.url === '/'){
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.write("Start\n");
+//     res.end("end");
+//     }
+//     else if(req.url === '/api/user'){
+//     const user = {
+//         name: 'kalash',
+//         age: 19,
+//         email: "kalash@gmail.com"
+//         }
+//     const data = JSON.stringify(user);
+//     console.log(JSON.parse(data));
+    
+//     res.writeHead(200,{'Content-Type':'application/json'});
+//     res.end(data);
+//     }else if(req.url === "/login"){
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end("Login successfully");
+//     }else if(req.url === "/signup"){
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end("Signup successfully");
+//     }else{
+//     res.writeHead(404,{'Content-Type':'text/html'});
+//     res.end("<h1>Page not found</h1>");    
+//     }
+// })
+
+
+// Handling a post method
+// GET , POST, DELETE , PUT , PATCH
 const http = require("http");
 const server = http.createServer((req,res) =>{
-    if(req.url === '/'){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.write("Start\n");
-    res.end("end");
-    }
-    else if(req.url === '/api/user'){
-    const user = {
-        name: 'kalash',
-        age: 19,
-        email: "kalash@gmail.com"
-        }
-    const data = JSON.stringify(user);
-    console.log(JSON.parse(data));
-    
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.end(data);
-    }else if(req.url === "/login"){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("Login successfully");
-    }else if(req.url === "/signup"){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("Signup successfully");
+if(req.method === "POST" && req.url === "/submit"){
+    let body = "";
+    req.on('data', (chunk)=>{
+        body += chunk.toString(); // Convert Buffer to string
+    })
+        // End event triggered when all data received
+    req.on('end',()=>{
+        console.log(JSON.parse(body));
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({success:true, message: "Account created successfully."}))
+    })
     }else{
-    res.writeHead(404,{'Content-Type':'text/html'});
-    res.end("<h1>Page not found</h1>");    
+        res.writeHead(404,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({success:false, message: "Not Found!"}))
+
     }
 })
 
---
+++ WEB_PRACTICE/website_3/server.js
@@ -0,0 +1,64 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+
+    // Serve HTML from pages folder
+    if (req.method === "GET" && req.url === "/") {
+        const filePath = path.join(__dirname, "pages", "index.html");
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end("Cannot load HTML");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            }
+        });
+    }
+
+    // Serve static JS files from public folder
+    else if (req.method === "GET" && req.url.startsWith("/public")) {
+        const filePath = path.join(__dirname, req.url);
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end("File not found");
+            } else {
+                res.writeHead(200, { "Content-Type": "application/javascript" });
+                res.end(data);
+            }
+        });
+    }
+
+    // Handle POST request
+    else if (req.method === "POST" && req.url === "/submit") {
+        let body = "";
+
+        req.on("data", chunk => body += chunk.toString());
+
+        req.on("end", () => {
+            const data = JSON.parse(body);
+            console.log("Received:", data);
+
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({
+                success: true,
+                message: "Login received"
+            }));
+        });
+    }
+
+    // 404
+    else {
+        res.writeHead(404);
+        res.end("Not Found");
+    }
+});
+
+server.listen(8000, () => {
+    console.log("Server running at http://localhost:8000");
+});
