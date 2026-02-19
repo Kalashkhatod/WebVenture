c907442b6bc598ec9fde899c8ece4a8bc725555f
Kalashkhatod <kalashkhatod@gmail.com>
2026-02-19 19:59:45 +0530

    Modified app.js

--- NODEJS/lesson2/11/app.js
+++ NODEJS/lesson2/11/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
 app.listen(8000, ()=>{
-     console.log('Server listen at port 8000');
+      console.log('Server listen at port 8000');
     
 });
