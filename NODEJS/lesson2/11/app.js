d7d81880f4164bdbf99e8e5872dfa49eebb045a1
Kalashkhatod <kalashkhatod@gmail.com>
2026-02-22 19:09:05 +0530

    Learning Expresss Js

--- NODEJS/lesson2/11/app.js
+++ NODEJS/lesson2/11/app.js
@@ -1,6 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+
 const app = express();
-app.listen(8000, ()=>{
-      console.log('Server listen at port 8000');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post("/api/v1/user/register", (req,res) => {
+      const obj = req.body;
+      console.log(obj);
+
+      res.status(200).json({
+            success: true,
+            message: "Account created successfully."
+      })
+})
+
+
+app.listen(PORT, ()=>{
+      console.log(`Server listen at port ${PORT}`); //`` used for dynamic data
     
 });
